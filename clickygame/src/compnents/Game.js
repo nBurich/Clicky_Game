535cffe45f4de7d4c73739618f1b6cc16ad0631b
nBurich <n.burich@hotmail.com>
2019-04-30 21:34:10 -0400

    not fully functioning

---
+++ clickygame/src/compnents/Game.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function ClickyGame(props) {
+    return(
+        <div>
+        <nav class="nav bar">
+            
+        </nav>
+        </div>
+        <div className="card">
+            <div
+        </div>
+    )
+}
+
+export default Game;
